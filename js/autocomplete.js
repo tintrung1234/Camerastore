8ca3b98400b271625dc471818add30aff7d16c9c
Hoàng Quý <2251120440@ut.edu.vn>
2024-07-24 17:17:53 +0700

    ubuhbhbih

---
+++ js/autocomplete.js
@@ -0,0 +1,18 @@
+let availableKeywords=[
+    "HTML",
+    "CSS",
+];
+
+const resultsBox = document.querySelector(".results-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+}
