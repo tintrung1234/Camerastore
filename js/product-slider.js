d30e1bb02fe61445c9d25104405ea3bc212f5c99
Tin Trung <huynhtrungtin2222@gmail.com>
2024-08-02 23:32:19 +0700

    display product

---
+++ js/product-slider.js
@@ -0,0 +1,53 @@
+"use strict";
+
+productScroll();
+
+function productScroll() {
+    const slider = document.getElementById("slider");
+    const next = document.getElementsByClassName("pro-next")[0]; // Get the first button
+    const prev = document.getElementsByClassName("pro-prev")[0]; // Get the first button
+    const slide = document.getElementById("product-group");
+
+    let position = 0; // Slider position
+
+    prev.addEventListener("click", function () {
+        // Click previous button
+        if (position > 0) {
+            position -= 1;
+            translateX(position); // Translate items
+        }
+    });
+
+    next.addEventListener("click", function () {
+        if (position < hiddenItems()) {
+            position += 1;
+            translateX(position); // Translate items
+        }
+    });
+
+    function hiddenItems() {
+        // Get hidden items
+        const items = getCount(slide, false);
+        const visibleItems = Math.floor(slider.offsetWidth / 210); // Adjust based on item width
+        return items - visibleItems; // Return the number of items that can be scrolled
+    }
+}
+
+function translateX(position) {
+    // Translate items
+    const slide = document.getElementById("product-group");
+    slide.style.transform = `translateX(${-position * 600}px)`; // Use transform for smoother animations
+}
+
+function getCount(parent, getChildrensChildren) {
+    // Count number of items
+    let relevantChildren = 0;
+    const children = parent.children; // Use children instead of childNodes for better performance
+    for (let i = 0; i < children.length; i++) {
+        if (getChildrensChildren) {
+            relevantChildren += getCount(children[i], true);
+        }
+        relevantChildren++;
+    }
+    return relevantChildren;
+}
--- js/product_cart.js
+++ js/product_cart.js
@@ -1,30 +1,93 @@
 //show product
 const product = [
     {
-        id: 0,
-        image: "./img/devImg.jpg",
-        title: "test Image",
-        price: 123412,
+        id: 1,
+        image: "./img/canon/200D/200D.png",
+        title: "Canon 200D",
+        price: 40000,
+        type: "canon",
+    },
+    {
+        id: 2,
+        image: "./img/canon/1500D-kit/1500D-kit.png",
+        title: "Canon 1500D-kit",
+        price: 43000,
+        type: "canon",
+    },
+    {
+        id: 3,
+        image: "./img/canon/3000D-kit/3000D-kit.png",
+        title: "Canon 3000D-kit",
+        price: 16000,
+        type: "canon",
+    },
+    {
+        id: 4,
+        image: "./img/canon/G7-X-mark/G7-X-mark.png",
+        title: "Canon G7-X-mark",
+        price: 23100,
+        type: "canon",
+    },
+    {
+        id: 5,
+        image: "./img/canon/phukien/speedlite.png",
+        title: "Đèn speedlite",
+        price: 5400,
+        type: "phukien",
+    },
+    {
+        id: 6,
+        image: "./img/canon/R7/R7.png",
+        title: "Canon R7",
+        price: 54322,
+        type: "canon",
+    },
+    {
+        id: 7,
+        image: "./img/canon/R8/R8.png",
+        title: "Canon R8",
+        price: 54323,
+        type: "canon",
+    },
+    {
+        id: 8,
+        image: "./img/canon/R50/R50.png",
+        title: "Canon R50",
+        price: 32554,
+        type: "canon",
+    },
+    {
+        id: 9,
+        image: "./img/canon/R100/R100.png",
+        title: "Canon R100",
+        price: 4325220,
+        type: "canon",
+    },
+    {
+        id: 10,
+        image: "./img/canon/SX740-HS/SX740-HS.png",
+        title: "Canon SX740-HS",
+        price: 540030,
         type: "canon",
     }
 ];
 
 const categories = [...new Set(product.map((item) => { return item; }))];
-let i = 0;
+let i = 1;
 var cartIcon = './img/carticon.png';
 
-document.getElementById("product").innerHTML = categories
+document.getElementById("product-group").innerHTML = categories
     .map((item) => {
         var { image, title, price } = item;
         return (
-            `<a href="product.html" class="productLink">
+            `<div id="product"><a href="product.html" class="productLink">
             <img class="productImg" src=${image} alt=""></a>
             <label class="productName" for="">${title}</label>
             <p class="productPrice">VND ${price}</p>` +
             `<button class='addCart' onclick='addtocart(` + i++ + `)'>
             <img src = "img/carticon.png" alt='cartIcon'> 
             <p class='muahang'> Mua hàng</p>
-            </button >`
+            </button > </div>`
         );
     }).join("");
 
