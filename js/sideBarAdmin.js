996545f224dd5984a37ed0d8ceff1e07b5d6a2ac
Tin Trung <huynhtrungtin2222@gmail.com>
2024-08-09 22:35:06 +0700

    admin dashboard, product

---
+++ js/sideBarAdmin.js
@@ -0,0 +1,35 @@
+document.getElementById("sideBarAdmin").innerHTML = `
+<div class="logo">
+            <a href="Home.html">
+                <img src="./img/ProCam.png" alt="" />
+            </a>
+        </div>
+        <div class="blockNav1">
+            <div class="dropdown">
+                <a href="admin.html">
+                    <button class="subBtn">
+                        Trang chủ
+                    </button>
+                </a>
+            </div>
+            <div class="dropdown">
+                <a href="productAdmin.html">
+                    <button class="subBtn">Sản phẩm</button>
+                </a>
+            </div>
+            <div class="dropdown">
+                <button class="subBtn">Phụ kiện</button>
+            </div>
+            <div class="dropdown">
+                <button class="subBtn">Sửa chữa</button>
+            </div>
+            <div class="dropdown">
+                <button class="subBtn">Khuyến mãi</button>
+            </div>
+            <div class="dropdown">
+                <button class="subBtn">Liên hệ</button>
+            </div>
+            <div class="dropdown">
+                <button class="subBtn">Bảo hành</button>
+            </div>
+        </div>`;
